e622517bb0f0fbbcc5d24fcea843c381473deb71
MukeshRameshTamil <mukesh200424@gmail.com>
2024-07-21 14:21:54 +0530

    Create combination sum.py

---
+++ combination sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+       
+        dp = [[] for _ in range(target + 1)]
+        dp[0].append([])
+
+        for i in range(1, target + 1):
+            for candidate in candidates:
+                if candidate <= i:
+                    for prev in dp[i - candidate]:
+                        temp = prev + [candidate]
+                        temp.sort()
+                        if temp not in dp[i]:
+                            dp[i].append(temp)
+        return dp[target]
