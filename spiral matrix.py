f24337a364a3d77a74821dfc96b9d42ddf09b062
MukeshRameshTamil <mukesh200424@gmail.com>
2024-07-22 18:30:28 +0530

    Create spiral matrix.py

---
+++ spiral matrix.py
@@ -0,0 +1,29 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+      
+        rows, cols = len(matrix), len(matrix[0])
+
+
+        row, col = 0, -1
+
+        direction = 1
+
+        result = []
+
+        while rows > 0 and cols > 0:
+           
+            for _ in range(cols):
+                col += direction
+                result.append(matrix[row][col])
+            rows -= 1
+
+           
+            for _ in range(rows):
+                row += direction
+                result.append(matrix[row][col])
+            cols -= 1
+
+            
+            direction *= -1
+
+        return result
